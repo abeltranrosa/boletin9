879065030792bde538b0cb2a6f50cfe080d2998a
antonio <antonio@biruseto>
2016-11-09 09:41:33 +0100

    boletin 9.1 main

---
+++ Boletin9.1/src/boletin9/pkg1/Boletin91.java
@@ -0,0 +1,16 @@
+
+package boletin9.pkg1;
+
+import javax.swing.JOptionPane;
+
+
+public class Boletin91 {
+
+    
+    public static void main(String[] args) {
+        
+        Contar obx = new Contar();
+        obx.contador();
+    }
+    
+}
