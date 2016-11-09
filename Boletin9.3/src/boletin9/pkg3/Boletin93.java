c40f0bf842b2d048f0249cc36041071229dc9807
antonio <antonio@biruseto>
2016-11-09 09:56:50 +0100

    boletin 9.3 main

---
+++ Boletin9.3/src/boletin9/pkg3/Boletin93.java
@@ -0,0 +1,13 @@
+
+package boletin9.pkg3;
+
+
+public class Boletin93 {
+
+    
+    public static void main(String[] args) {
+        Area obxoperacion = new Area();
+        obxoperacion.Rectangulo();
+    }
+    
+}
