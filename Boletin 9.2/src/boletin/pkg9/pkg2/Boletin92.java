0397e9989d081fa9c24f8320d6d8ea06ccb9aa86
antonio <antonio@biruseto>
2016-11-09 09:42:33 +0100

    boletin 9.2 main

---
+++ Boletin 9.2/src/boletin/pkg9/pkg2/Boletin92.java
@@ -0,0 +1,12 @@
+
+package boletin.pkg9.pkg2;
+
+
+public class Boletin92 {
+
+   
+    public static void main(String[] args) {
+        Operacion obxcalculo = new Operacion();
+        obxcalculo.Operando();
+    }
+}
