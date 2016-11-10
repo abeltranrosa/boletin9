0d69bdc9e3ef113a1066b495fddc3f91504d70db
antonio <antonio@biruseto>
2016-11-10 10:36:52 +0100

    boletin 9.4 main

---
+++ Boletin9.4/src/boletin9/pkg4/Boletin94.java
@@ -0,0 +1,13 @@
+
+package boletin9.pkg4;
+
+
+public class Boletin94 {
+
+    
+    public static void main(String[] args) {
+        Numero obxmultiplica = new Numero();
+        obxmultiplica.tabla();
+    }
+    
+}
