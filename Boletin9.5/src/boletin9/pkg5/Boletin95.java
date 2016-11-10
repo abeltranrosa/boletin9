aee32e3775ed142a13ee044b4ac24c0e4cf996c7
antonio <antonio@biruseto>
2016-11-10 12:07:02 +0100

    boletin 9.5 main con los apartados a,b,c

---
+++ Boletin9.5/src/boletin9/pkg5/Boletin95.java
@@ -0,0 +1,20 @@
+
+package boletin9.pkg5;
+
+
+public class Boletin95 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("esto es la cadena de los pares");
+        Elementos obxoperacion = new Elementos();
+        obxoperacion.operacion(10);
+        System.out.println("esto es la cadena de numeros positivos y negativos");
+        Elementopositivonegativo obxOperacion = new Elementopositivonegativo();
+        obxOperacion.operacion();
+        System.out.println("esto es fibonacci");
+        Fibonacci obxperacion = new Fibonacci();
+        obxperacion.operacion();
+    }
+    
+}
