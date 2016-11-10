cd6249eaa3762bc2e13078681579c064a9c9993c
antonio <antonio@biruseto>
2016-11-10 12:05:31 +0100

    boletin 9.5 clase Elementos

---
+++ Boletin9.5/src/boletin9/pkg5/Elementos.java
@@ -0,0 +1,18 @@
+
+package boletin9.pkg5;
+
+
+public class Elementos {
+    public void operacion (int npar){
+            int acumuladorsuma=0;
+            int i =0;
+            while (i<npar){
+                acumuladorsuma =acumuladorsuma+2;
+                    System.out.println(acumuladorsuma);
+                    i++;
+                
+                }
+            }
+    
+    
+    }   
